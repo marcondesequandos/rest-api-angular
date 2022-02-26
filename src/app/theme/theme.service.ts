ea2eb8e1f9b548a0cc75131098153ecc4c3eb4ec
marcondesequandos <felipemgcarvalho@gmail.com>
2022-02-26 14:42:47 -0300

    Criação do serviço para alterar o tema

---
+++ src/app/theme/theme.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+export enum Theme {
+  light = 'light',
+  dark = 'dark',
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+  private mode: BehaviorSubject<Theme> = new BehaviorSubject<Theme>(Theme.light);
+
+  constructor() {}
+
+  get mode$(): Observable<Theme> {
+    return this.mode.asObservable();
+  }
+
+  toggleMode() {
+    if (this.mode.value === Theme.light) {
+      this.mode.next(Theme.dark);
+    } else {
+      this.mode.next(Theme.light)
+    }
+    console.log(this.mode)
+  }
+}
